ddafa00c101e9fefe22bda5e0575bb7a80f97e50
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-01-05 18:56:56 +0530

    shop button issue fixed

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -46,7 +46,7 @@ function Cart() {
         ) : (
           <div className="flex flex-col h-screen justify-center items-center m-auto">
             <h1 className="text-red-500 font-bold text-2xl p-4 m-4">Cart Is Empty</h1>
-            <Link to={"/"}>
+            <Link to={"/home"}>
               <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                 Shop Now
               </button>
